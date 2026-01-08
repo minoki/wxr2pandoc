332fcecaea4ae3c3804370446299920aaebca7b8
ARATA Mizuki <minorinoki@gmail.com>
2026-01-08 22:20:15 +0900

    Apply GPL-2.0-or-later

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,21 @@
+{-
+wxr2pandoc
+Copyright (C) 2026  ARATA Mizuki
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see
+<https://www.gnu.org/licenses/>.
+-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
